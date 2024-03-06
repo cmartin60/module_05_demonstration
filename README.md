eaa8e93766c00a48c16faff1922a82f256ec820b
Christian Martin <cmartin60@academic.rrc.ca>
2024-03-06 17:24:18 -0600

    Initial Commit: README Files and functions scripts added to file

---
+++ README.md
@@ -0,0 +1,17 @@
+## Module 05 Demonstration Part 1
+
+## Description
+Introduction to Python Functions
+
+## Author
+Christian Jorge Martin
+
+## Demonstration Topics
+Functions
+Benefits of using functions
+Function definition and syntax
+Function parameters
+Function return values
+Exceptions in functions
+Main guard
+Function Docstrings
---
+++ functions.py
@@ -0,0 +1,24 @@
+"""
+Description: Module 05 demonstration: Functions
+Author: {student name}
+Date: {current date}
+Usage: To run individual functions, place a function 
+call within the main guard.
+
+Function: Reusable piece/block of code
+Most functions can take different inputs and return different outputs
+
+Reasons for functions:
+Modularity: single large or complex mechanism can broken down into manageable pieces
+
+Reusability: DONT REPEAT YOURSELF (DRY)
+
+Maintainability: clear organization, easier to diagnose problems
+Functions should do ONE thing
+Single source of truth
+Don't define functions within other functions
+
+Debugging: automatically unit test functions
+
+Abstraction: "information hiding"
+"""
